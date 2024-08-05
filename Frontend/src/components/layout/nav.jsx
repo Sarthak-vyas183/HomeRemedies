f3d640d5b7d9b004c5c630d1797fd9b062f9bb68
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-08-05 18:35:01 +0530

    home page design part-01 done

--- Frontend/src/components/layout/nav.jsx
+++ Frontend/src/components/layout/nav.jsx
@@ -7,29 +7,29 @@ function Nav() {
 
   return (
     <div>
-      <div className='w-[100vw] h-[10vh] bg-blue-900 fixed z-10'>
-        <div className='h-full flex justify-evenly items-center text-white'>
-          <h1>Home Remedies</h1>
+      <div className='w-[100vw] h-[10vh] bg-transparent  fixed z-10 '>
+        <div className='h-full flex justify-evenly items-center text-white  font-semibold text-2xl font-semibold '>
+          <h1 className='text-gray-100'>HomeRemedy.in</h1>
           <ul className='flex gap-10'>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/remedies">Remedies</Link></li>
+            <li className=' hover:text-yellow-300'><Link to="/">Home</Link></li>
+            <li className=' hover:text-yellow-300'><Link to="/about">About</Link></li>
+            <li className=' hover:text-yellow-300'><Link to="/contact">Contact</Link></li>
+            <li className=' hover:text-yellow-300'><Link to="/remedies">Remedies</Link></li>
             {isLoggedin ? (
               <>
                 {user && user.isAdmin ? (
-                  <li><Link to="/admin/profile">Profile</Link></li>
+                  <li className='hover:text-yellow-300'><Link to="/admin/profile">Profile</Link></li>
                 ) : user && user.isDoctor ? (
-                  <li><Link to="/doctor/profile">Profile</Link></li>
+                  <li className='hover:text-yellow-300'><Link to="/doctor/profile">Profile</Link></li>
                 ) : (
-                  <li><Link to="/user/profile">Profile</Link></li>
+                  <li className='hover:text-yellow-300'><Link to="/user/profile">Profile</Link></li>
                 )}
-                <li><Link to="/logout">Logout</Link></li>
+                <li className='hover:text-yellow-300'><Link to="/logout">Logout</Link></li>
               </>
             ) : (
               <>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">SignUp</Link></li>
+                <li className='hover:text-yellow-300'><Link to="/login">Login</Link></li>
+                <li className='hover:text-yellow-300'><Link to="/signup">SignUp</Link></li>
               </>
             )}
           </ul>
--- Frontend/src/components/pages/home.jsx
+++ Frontend/src/components/pages/home.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 function home() {
   return (
-    <div className='w-[100vw] h-[90vh] bg-green-400'>
-      <h1>Home page</h1>
+    <div className='w-[100vw] h-[100vh] ' style={{backgroundImage : 'url(../../../images/home.png)', backgroundSize:'cover', backgroundRepeat : 'no-repeat' , rotate : '360deg'}}>
+         <section className="left w-[50%] h-full  flex flex-col justify-center items-center">
+                <div className="w-full px-16 ">
+                   <p className='text-green-400 bg-gray-700 bg-opacity-80 rounded-md p-2 text-5xl'>Natural Herbal <br/>Remedies</p>
+                </div>
+
+                <div className="w-full  px-16">
+                   <h1 className='text-white text-4xl font-bold'>100% <br /> Organic</h1>
+                </div>
+                  <br /><br />
+                <div className="w-full flex gap-2 px-16">
+                   <span className='w-1 h-[100%] bg-red-500 rounded-sm'></span>
+                   <p className='text-white w-[70%]'>Rural remedies epitomize a harmonious blend of nature's wisdom and human resilience, providing holistic healing solutions rooted in centuries-old knowledge. They celebrate the enduring efficacy of traditional practices, showcasing a sustainable approach to wellness deeply intertwined with community strength.</p>  
+                </div>   
+         </section>
     </div>
   )
 }
